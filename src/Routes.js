6e6b45e5e2f95dc0e653898f2c4aa25c12ea6a55
Gabriel Barreto <gabriel.barret015@hotmail.com>
2019-10-29 21:05:35 -0300

    adicionado os metodos get,post,delete e put.

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,11 @@ const routes = express.Router();
 const ClientController = require("./controllers/ClientController");
 
 routes.get("/clients", ClientController.index);
+routes.get("/clients/:id", ClientController.show);
+routes.put("/clients/:id", ClientController.update);
 routes.post("/clients", ClientController.add);
+routes.delete("/clients/:id", ClientController.delete);
+
 
 
 module.exports = routes;
--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -12,5 +12,20 @@ module.exports = {
         const clients = await Client.create(req.body);
 
         return res.json(clients);
+    }, 
+    async update(req, res){
+        const clients = await Client.findByIdAndUpdate(req.params.id, req.body, {new:true});
+
+        return res.json(clients);
+    },
+    async show(req, res){
+        const clients = await Client.findById(req.params.id);
+
+        return res.json(clients);
+    },
+    async delete(req, res){
+        await Client.findOneAndRemove(req.params.id);
+
+        return res.send();
     }
 }
